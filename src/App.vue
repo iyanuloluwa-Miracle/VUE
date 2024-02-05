4d585239d45d4494862197ad2aba72fb09a91b7a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-05 09:10:23 +0100

    binding text

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <div>{{ greet }} {{ name }}</div>
+    <div v-text="channel"></div>
+  </div>
+</template>
+
+
+<script>
+export default {
+  name: 'App',
+  data(){
+    return{
+      name:'Iyanu',
+      greet:"Hello",
+      channel:'codevolution'
+    }
+  }
+  
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 100px;
+}
+</style>
