85f9e4d54d5644e176cf423578d5a4dc9740272d
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-05 11:07:11 +0100

    Binding Classes

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,18 @@
 <template>
   <div>
-    <div>{{ greet }} {{ name }}</div>
-    <div v-html="channel"></div>
     <h2 v-bind:id="headingId">Heading</h2>
     <button v-bind:disabled="isDisabled">bind</button>
+    <h2 class="underline">Underlined text</h2>
+    <h2 class="underline" v-bind:class="status">Status</h2>
+    <h1 v-bind:class="isPromoted && 'promoted'">Promoted Movie</h1>
+    <h2 v-bind:class="isSoldOut ? 'sold-out':'new' ">Soldout? movie</h2>
+    <h2 v-bind:class="['new', 'promoted']">Newly promoted movie</h2>
+    <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'sold-out':'new']">Array conditional</h2>
+     <h2 v-bind:class="{
+      promoted: isPromoted,
+      new: !isSoldOut,
+      'sold-out': isSoldOut
+     }">Object Conditional movie</h2>
   </div>
 </template>
 
@@ -17,7 +26,10 @@ export default {
       greet:"Hello",
       channel:'codevolution',
       headingId:'heading',
-      isDisabled:false
+      isDisabled:false,
+      status:'danger',
+      isPromoted:false,
+      isSoldOut:false
     }
   }
   
@@ -27,10 +39,26 @@ export default {
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 100px;
+}
+
+.underline{
+  text-decoration: underline;
+
+}
+
+.promoted{
+  font-style: italic;
+}
+
+.new{
+  color: crimson;
+}
+.sold-out {
+  color: blue;
+
+
 }
 </style>
