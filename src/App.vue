103cf474eddcc24dc193d6e80c149ab277ea8a2a
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-08 23:04:10 +0100

    computed properties

--- src/App.vue
+++ src/App.vue
@@ -1,84 +1,54 @@
 <template>
-  <form>
-    <div>
-      <pre>
-        {{JSON.stringify(formValues, null,2)}}
-      </pre>
-    </div>
-    <div>
-      <label for="name">Name</label>
-      <input type="text" id="name" v-model="formValues.name" />
-    </div>
-
-    <div>
-      <label for="profile">Profile Summary</label>
-      <textarea id="profile" v-model="formValues.profileSummary" />
-    </div>
-
-
-    <div>
-      <label for="country">Country</label>
-      <select id="country" v-model="formValues.country">
-        <option value="">Select a country</option>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <label for="job-location">Job Location</label>
-      <select id="job-location" multiple v-model="formValues.joblocation">
-        <option value="">Select a country</option>
-        <option value="india">India</option>
-        <option value="vietnam">Vietnam</option>
-        <option value="singapore">Singapore</option>
-      </select>
-    </div>
-
-    <div>
-      <input type="checkbox" id="remoteWork" v-model="formValues.remoteWork" true-value="yes" false-value="no"/>
-      <label for="remoteWork">Open to remote work</label>
-    </div>
-
-    <div>
-      <label>Skill set</label>
-      <input type="checkbox" id="html" value="html"/>
-
-      <label for="html">HTML</label>
-      <input type="checkbox" id="html" value="html" v-model="formValues.skillSet"/>
-      <label for="css">Css</label>
-      <input type="checkbox" id="css" value="css" v-model="formValues.skillSet" />
-      <label>Javascript</label>
-      <input type="checkbox" id="javascript" value="javascript" v-model="formValues.skillSet"/>
-    </div>
-  </form>
- 
+  <div>
+    <h2>Computed fullname - {{ fullName }}</h2>
+    <button @click="items.push({ id: 1, title: 'keyboard', price: 5 })">
+      Add Item
+    </button>
+    <h2>Computed Total - {{ total }}</h2>
+    <h2>Method Total - {{ getTotal() }}</h2>
+  </div>
 </template>
-
-
 
 <script>
 export default {
   name: "App",
   data() {
     return {
-      formValues:{
-        name:'',
-        profileSummary:'',
-        country: '',
-        joblocation:[],
-        remoteWork:"no",
-        skillSet:[],
-        
-      }
-         
+      firstName: "Bruce",
+      lastName: "Wayne",
+      items: [
+        {
+          id: 1,
+          title: "Tv",
+          price: 100,
+        },
+        {
+          id: 2,
+          title: "phone",
+          price: 200,
+        },
+        {
+          id: 3,
+          title: "Laptop",
+          price: 300,
+        },
+      ],
     };
   },
-  methods:{
-   
-   
-  }
+  methods: {
+    getTotal(){
+      return this.items.reduce((total, curr) => total + curr.price, 0);
+
+    }
+  },
+  computed: {
+    fullName() {
+      return `${this.firstName} ${this.lastName}`;
+    },
+    total() {
+      return this.items.reduce((total, curr) => total + curr.price, 0);
+    },
+  },
 };
 </script>
 
@@ -88,30 +58,5 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 100px;
-}
-label{
-  font-weight: bold;
-  display: flex;
-  margin-bottom: 5px;
-}
-
-input + label{
-  font-weight: bold;
-  display: flex;
-  margin-right: 5px;
-  
-}
-input[type='text'],
-textarea,
-select{
-  display: block;
-  width:400px;
-  padding: 6px 12px;
-  font-size: 14px;
-  line-height: 1.42857143;
-  color: hotpink;
-  background-color: pink ;
-  background-image: none;
-
 }
 </style>
