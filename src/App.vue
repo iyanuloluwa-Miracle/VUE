578415d31a46561497468ecfddad42648b51f7ee
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-09 11:39:12 +0100

    learning watchers

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,14 @@
 <template>
-  <div>
-    <h2>Computed fullname - {{ fullName }}</h2>
-    <button @click="changeFullName">Change fullName</button>
-    
-    
-    
-    <button @click="items.push({ id: 1, title: 'keyboard', price: 5 })">
-      Add Item
-    </button>
-    <h2>Computed Total - {{ total }}</h2>
-    <h2>Method Total - {{ getTotal() }}</h2>
-
-
-    <div v-for="item in items" :key="item.id">
-      <h2 v-if="item.price > 100">{{ item.title }} {{ item.price }}</h2>
-    </div>
-
+  <div id="app">
+    <h2>Volume Tracker(0-20)</h2>
+    <h3>Current Volume - {{ volume }}</h3>
     <div>
-      <h2 v-for="item in expensiveItems" :key="item.id"> {{ item.title }} {{ item.price }}</h2>
+      <button @click="volume +=2">Increase</button>
+      <button @click="volume -=2">decrease</button>
     </div>
 
-    
-
-    
   </div>
+ 
   
 </template>
 
@@ -32,59 +17,19 @@ export default {
   name: "App",
   data() {
     return {
-      firstName: "Bruce",
-      lastName: "Wayne",
-      items: [
-        {
-          id: 1,
-          title: "Tv",
-          price: 100,
-        },
-        {
-          id: 2,
-          title: "phone",
-          price: 200,
-        },
-        {
-          id: 3,
-          title: "Laptop",
-          price: 300,
-        },
-      ],
+      volume: 0
     };
   },
-  methods: {
-    getTotal(){
-      return this.items.reduce((total, curr) => total + curr.price, 0);
-
-    },
-
-    changeFullName(){
-        this.fullName = 'Clark Kent'
-
+  methods: {},
+  computed: {},
+  watch:{
+    volume(newValue, oldValue){
+      if(newValue > oldValue && newValue === 16){
+        alert('listening to a high volume for a long time may damage your hearing')
       }
-  },
-  computed: {
-    fullName: {
-      get() {
-        return `${this.firstName} ${this.lastName}`;
-      },
-      set(value) {
-        const names = value.split(' ')
-        this.firstName = names[0]
-        this.lastName = names[1]
 
-      },
-     
-     
-    },
-    total() {
-      return this.items.reduce((total, curr) => total + curr.price, 0);
-    },
-    expensiveItems() {
-      return this.items.filter((item) => item.price > 100);
-    },
-  },
+    }
+  }
 };
 </script>
 
