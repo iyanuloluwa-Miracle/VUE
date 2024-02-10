b060edd92aace1a8dab22b275a8e9afd56684a81
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-10 11:43:48 +0100

    learning about components

--- src/App.vue
+++ src/App.vue
@@ -1,61 +1,18 @@
 <template>
   <div id="app">
-    <h2>Volume Tracker(0-20)</h2>
-    <h3>Current Volume - {{ volume }}</h3>
-    <div>
-      <button @click="volume +=2">Increase</button>
-      <button @click="volume -=2">decrease</button>
-    </div>
-
-    <input type="text" v-model="movie" >
-    <input type="text" v-model="movieInfo.title" >
-    <input type="text" v-model="movieInfo.actor" >
-
+    <GreetMessage></GreetMessage>
   </div>
- 
-  
 </template>
 
 <script>
+import GreetMessage from './components/GreetMessage.vue';
+
 export default {
   name: "App",
-  data() {
-    return {
-      volume: 0,
-      movie: 'Batman',
-      movieInfo: {
-        title: '',
-        actor:''
-      }
-    };
-  },
-  methods: {},
-  computed: {},
-  watch: {
-    volume(newValue, oldValue) {
-      if (newValue > oldValue && newValue === 16) {
-        alert('Listening to a high volume for a long time may damage your hearing');
-      }
-    },
-    movie: {
-      handler(newValue) {
-        console.log(`Calling API with name=${newValue}`);
-      },
-      immediate: true,
-      //deep: true // Assuming movie is an object and you want to watch for nested changes
-    }
-  },
-  movieInfo:{
-    handler(newValue){
-      console.log(
-        `Calling API with movie title = ${newValue.title} and actor = ${newValue.actor}`
-      )
-
-    },
-    deep: true
+  components:{
+    GreetMessage
   }
 };
-
 </script>
 
 <style>
---
+++ src/components/GreetMessage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h2>Hello Batman</h2>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'GreetMessage'
+}
+</script>
+
+<style scoped>
+
+</style>
