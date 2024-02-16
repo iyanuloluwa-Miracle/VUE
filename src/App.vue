042eafdf2d65cd524961ed8748816a630e7de412
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-16 13:00:20 +0100

    Named slots

--- src/App.vue
+++ src/App.vue
@@ -8,7 +8,22 @@
     <Card>
       <img src="https://picsum.photos/200"/>
     </Card>
-    
+    <Card>
+      <template v-slot:header>
+        <h3>Header</h3>
+
+      </template>
+      <template v-slot:default>
+        <img src="https://picsum.photos/200"/>
+        
+
+      </template>
+      <template v-slot:footer>
+       <button>View Details</button>
+
+      </template>
+      
+    </Card>
   </div>
 </template>
 
