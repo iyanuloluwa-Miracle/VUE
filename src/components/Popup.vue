662fcb219b55918dccde10bd7247e0e9e280c70d
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-16 09:03:36 +0100

    validating emitters

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,14 +1,30 @@
 <template>
     <div>
         <h2>This is a Popup</h2>
-        <button @click="$emit('close', 'Iyanuloluwa')"> Close Popup</button>
+        <input type="text" v-model="name"/>
+        <button @click="$emit('close', name)"> Close Popup</button>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Popup',
-        emits: ['close']
+        emits: {
+            close:(name) =>{
+                if(!name){
+                    return false
+                     
+                } else{
+                    return true
+                }
+
+            }
+        },
+        data(){
+            return{
+                name: ''
+            }
+        }
         
     }
 </script>
