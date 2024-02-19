330e57eb717b228d0aa6ffbc040c91124f0896bd
iyanuloluwa-Miracle <iyanudina@gmail.com>
2024-02-19 12:36:24 +0100

    Added slot props

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,49 @@
 <template>
   <div id="app">
-    <Card></Card>
-    <Card>Card Content</Card>
-    <Card>
-      <h2>Card Content</h2>
-    </Card>
-    <Card>
-      <img src="https://picsum.photos/200"/>
-    </Card>
-    <Card>
-      <template v-slot:header>
-        <h3>Header</h3>
-
-      </template>
-      <template v-slot:default>
-        <img src="https://picsum.photos/200"/>
-        
-
-      </template>
-      <template v-slot:footer>
-       <button>View Details</button>
+    <NameList>
+      <template v-slot:default="slotProps">
+        {{ slotProps.firstName }} {{ slotProps.lastName }}
 
       </template>
       
-    </Card>
+    </NameList> 
+
+
+    <NameList>
+      <template v-slot:default="slotProps">
+        {{ slotProps.firstName }} {{ slotProps.lastName }}
+
+      </template>
+      
+    </NameList>
+
+    <NameList>
+      <template v-slot:default="slotProps">
+        {{ slotProps.lastName }}
+
+      </template>
+      
+    </NameList> 
+
+    <NameList>
+      <template v-slot:default="slotProps">
+        {{ slotProps.firstName }}
+
+      </template>
+      
+    </NameList> 
   </div>
 </template>
 
 <script>
-import Card from './components/Card.vue';
+//import Card from './components/Card.vue';
+import NameList from './components/NameList.vue';
 
 export default {
   name: "App",
   components:{
-    Card,
+    
+    NameList
     
   },
 
